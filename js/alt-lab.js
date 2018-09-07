8241f59194c3cd66f0531a57eb85a3365b0df383
Tom Woodward <twwoodward@me.com>
2018-09-07 10:21:37 -0400

    switch radio button rating style

--- js/alt-lab.js
+++ js/alt-lab.js
@@ -32,23 +32,41 @@ ratings.forEach(function(element) {
 
 //rating averages 
 
-function averageRatings(category){
-	var total = 0;
-	var count = 0;
-	var avg = 0;
-	var dataDivs = document.getElementsByClassName(category+'-data');
-	dataDivs = Array.from(dataDivs);
-	dataDivs.forEach(function(div){		
-		total = parseInt(div.innerHTML) + total;
-		console.log(total);
-		count++;
-		console.log(count);
-		avg = total/count;		
-	})
-	var destination = document.getElementById(category+'-avg');
-	destination.innerHTML = avg;
-}
+// function averageRatings(category){
+// 	var total = 0;
+// 	var count = 0;
+// 	var avg = 0;
+// 	var dataDivs = document.getElementsByClassName(category+'-data');
+// 	dataDivs = Array.from(dataDivs);
+// 	dataDivs.forEach(function(div){		
+// 		total = parseInt(div.innerHTML) + total;
+// 		console.log(total);
+// 		count++;
+// 		console.log(count);
+// 		avg = total/count;		
+// 	})
+// 	var destination = document.getElementById(category+'-avg');
+// 	destination.innerHTML = avg;
+// }
 
-averageRatings('drawing');
-averageRatings('design');
-averageRatings('rendering');
+// averageRatings('drawing');
+// averageRatings('design');
+// averageRatings('rendering');
+
+
+//listen to radio buttons
+jQuery(document).ready(function(){
+    jQuery('input[type=radio]').click(function(){
+        var id = this.id;
+        setRadioChildren(this.value,this.id);
+    });
+});
+
+function setRadioChildren(num, id){
+	var idStem = id.substring(6, id.length -1);
+	var labelStem = 'label'+idStem;
+	for (i = 1; i < num; i++) {
+		var target = document.getElementById(labelStem+(num-i-1));
+		target.classList.add("chosen-child");
+	}
+}
